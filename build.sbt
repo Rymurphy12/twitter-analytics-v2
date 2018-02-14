3a44628734c73eb080e88715170d4b0dc4935290
rymurphy12 <rymurphy12@gmail.com>
2018-02-14 16:27:28 -0600

    Finished writing the example from http4s.org. Next step is to experiment to gain a better understanding of the libraries and then to eventually implement the actual solution.

--- build.sbt
+++ build.sbt
@@ -9,7 +9,11 @@ scalaVersion := "2.12.4" // Also supports 2.11.x
 val http4sVersion = "0.18.0"
 
 // Only necessary for SNAPSHOT releases
-resolvers += Resolver.sonatypeRepo("snapshots")
+resolvers ++=Seq(Resolver.sonatypeRepo("snapshots"),
+                 Resolver.sonatypeRepo("releases"),
+                 Resolver.typesafeIvyRepo("releases")
+)
+
 
 libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-dsl" % http4sVersion,
@@ -17,7 +21,10 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-blaze-client" % http4sVersion,
   "org.http4s" %% "http4s-circe" % http4sVersion,
   "io.circe" %% "circe-generic" % "0.9.1",
-  "io.circe" %% "circe-literal" % "0.9.1"
+  "io.circe" %% "circe-literal" % "0.9.1",
+  "com.typesafe" % "config" % "1.3.1",
+  "com.vdurmont" % "emoji-java" % "3.3.0",
+  "com.github.nscala-time" %% "nscala-time" % "2.18.0"
 )
 
 scalacOptions ++= Seq("-Ypartial-unification")
--- src/main/scala/com/github/rymurphy12/analyzer/TwitterMetrics.scala
+++ src/main/scala/com/github/rymurphy12/analyzer/TwitterMetrics.scala
@@ -10,5 +10,36 @@ import fs2.io.stdout
 import fs2.text.{lines, utf8Encode}
 import jawnfs2._
 import io.circe.Json
+import com.typesafe.config.ConfigFactory
+import org.joda.time.DateTime
+import com.vdurmont.emoji._
+import com.github.nscala_time.time.Imports._
 
-abstract class
+object TWStream extends TWStreamApp[IO]{
+  
+
+}
+
+abstract class TWStreamApp[F[_]: Effect] extends StreamApp[F]{
+
+  implicit val f = io.circe.jawn.CirceSupportParser.facade
+
+  def sign(consumerKey: String, consumerSecret: String, accessToken: String, accessSecret: String)(req: Request[F]): F[Request[F]] = {
+    val consumer = oauth1.Consumer(consumerKey, consumerSecret)
+    val token = oauth1.Token(accessToken, accessSecret)
+    oauth1.signRequest(req, consumer, callback = None, verifier = None, token = Some(token))
+  }
+
+  def jsonStream(consumerKey: String, consumerSecret: String, accessToken: String, accessSecret: String)(req: Request[F]): Stream[F, Json] =
+    for {
+      client <- Http1Client.stream[F]()
+      sr <- Stream.eval(sign(consumerKey, consumerSecret, accessToken, accessSecret)(req))
+      res <- client.streaming(sr)(resp => resp.body.chunks.parseJsonStream)
+    } yield res
+
+  override def stream(args: List[String], requestShutdown: F[Unit], consumerKey: String, consumerSecret: String, accessToken: String, accessSecret: String): Stream[F, ExitCode] = {
+    val req = Request[F](Method.GET, Uri.uri("https://stream.twitter.com/1.1/statuses/sample.json"))
+    val s = jsonStream(consumerKey, consumerSecret, accessToken, accessSecret)(req)
+    s.map(_.spaces2).through(lines).through(utf8Encode).to(stdout) >> Stream.emit(ExitCode.Success)
+  }
+}
